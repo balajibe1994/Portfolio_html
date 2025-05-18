f80d0e310cdcfd3247650c99cbbb20646006fc37
balajibe1994 <89129039+balajibe1994@users.noreply.github.com>
2025-05-18 18:22:06 +0530

    mobile view code changes

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function scrollToContact() {
   document.getElementById('Contact').scrollIntoView({ behavior: 'smooth' });
 }
 
+function scrollToContactAndClose() {
+  document.getElementById('Contact').scrollIntoView({ behavior: 'smooth' });
+  toggleNav(); // Close the drawer after clicking "Contact Me"
+}
+
+
 function downloadCV() {
   const link = document.createElement('a');
   link.href = '/assets/balaji_krishnan_cv.pdf'; // Replace with your actual file path
@@ -21,4 +27,13 @@ function sendEmail(event) {
 
   window.location.href = `mailto:balajibe1994@gmail.com?subject=${subject}&body=${body}`;
   return false;
+}
+
+function toggleNav() {
+  const mobileNavDrawer = document.getElementById("mobileNavDrawer");
+  if (mobileNavDrawer.style.width === "0px" || mobileNavDrawer.style.width === "") {
+    mobileNavDrawer.style.width = "70%"; // Open the drawer
+  } else {
+    mobileNavDrawer.style.width = "0px"; // Close the drawer
+  }
 }
